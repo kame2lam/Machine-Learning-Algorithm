4c0ed4783407ad2b45a669f7cbd076cca063fec1
kame2lam <kame2lam@gmail.com>
2021-02-11 00:03:09 +0800

    update

---
+++ 8.神經網絡分類算法/ch8-1.py
@@ -0,0 +1,11 @@
+#ch8.1.py
+from sklearn.datasets import load_iris 
+#從Scikit-Learn庫導入神經網路模型中的神經網路分類演算法 
+from sklearn.neural_network import MLPClassifier 
+#載入鳶尾花資料集 
+X, y = load_iris(return_X_y=True) 
+#訓練模型 
+clf = MLPClassifier().fit(X, y) 
+#使用模型進行分類預測 
+print(clf.predict(X))
+print('性能:',clf.score(X,y))
